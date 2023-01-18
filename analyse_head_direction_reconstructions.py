e465de2cc3f4b0c0b1da9a9f521d76c232316670
TomKnowles1994 <71404211+TomKnowles1994@users.noreply.github.com>
2023-01-18 12:59:17 +0000

    Update analyse_head_direction_reconstructions.py

--- analyse_head_direction_reconstructions.py
+++ analyse_head_direction_reconstructions.py
@@ -22,6 +22,10 @@ reconstructions_folder = "C:/Users/Tom/Downloads/HBP/representations/NRP/whiskey
 #reconstructions = np.load(reconstructions_folder + 'reconstructions_hd_cells.npy')
 reconstructions = np.load(reconstructions_folder + 'reconstructions_head_direction.npy')
 
+reconstructions_folder = "C:/Users/Tom/Downloads/HBP/representations/NRP/whiskeye_guifen_vis_hd_rat_all/real_data_6005_timestamped/visual/"
+
+reconstructions_2 = np.load(reconstructions_folder + 'reconstructions_head_direction.npy')[:290]
+
 print(data.shape)
 print(reconstructions.shape)
 
@@ -33,27 +37,46 @@ non_zero_rows = np.unique(np.nonzero(data)[0])
 #     plt.plot(np.arange(data.shape[1]), reconstructions[i,:])
 #     plt.show()
 
-fig, ax = plt.subplots(5,1, gridspec_kw={'height_ratios':[3,1,1,1,1]})
+fig, ax = plt.subplots(5,1, gridspec_kw={'height_ratios':[3,1,1,1,1]}, figsize = (20, 15))
+
+plt.subplots_adjust(left=0.125, bottom=None, right=0.9, top=None, wspace=None, hspace=None)
 
 a1 = ax[0].plot(np.arange(data.shape[1]), data[0,:], label = 'Test Data')[0]
-a2 = ax[0].plot(np.arange(data.shape[1]), reconstructions[0,:], label = 'Reconstruction')[0]
-a3 = ax[0].axvline(np.argmax(reconstructions[0,:]), 0, 1, color = 'green', label = 'Reconstruction Peak')
+a2 = ax[0].plot(np.arange(data.shape[1]), reconstructions[0,:], label = 'Reconstruction (Centre)')[0]
+a3 = ax[0].axvline(np.argmax(reconstructions[0,:]), 0, 1, color = 'purple', label = 'Reconstruction Peak (Centre)')
 a4 = ax[0].text(0, 0.02, "{}/{}".format(0, len(data)))
-#ax.set_ylim(0, 1)
+a5 = ax[0].plot(np.arange(data.shape[1]), reconstructions_2[0,:], label = 'Reconstruction (All)')[0]
+a6 = ax[0].axvline(np.argmax(reconstructions_2[0,:]), 0, 1, color = 'red', label = 'Reconstruction Peak (All)')
 ax[0].set_title("Train: Centre Perspective Rotation; Test: Real Rat")
 
 mse = np.mean((data - reconstructions) ** 2, axis = 1)
-print(mse.shape)
-distance_from_max = np.abs(np.argmax(data, axis = 1) - np.argmax(reconstructions, axis = 1))
-print(distance_from_max.shape)
-roughness = np.mean(np.diff(reconstructions, axis = 1), axis = 1)
-print(roughness.shape)
+mse_2 = np.mean((data - reconstructions_2) ** 2, axis = 1)
+distance_from_max_0 = np.abs(np.argmax(data, axis = 1) - np.argmax(reconstructions, axis = 1))
+distance_from_max_1 = np.abs((np.argmax(data, axis = 1) + 180) - np.argmax(reconstructions, axis = 1))
+distance_from_max_2 = np.abs((np.argmax(data, axis = 1) - 180) - np.argmax(reconstructions, axis = 1))
+distance_from_max = np.min([distance_from_max_0, distance_from_max_1, distance_from_max_2], axis = 0)
+distance_from_max_2_0 = np.abs(np.argmax(data, axis = 1) - np.argmax(reconstructions_2, axis = 1))
+distance_from_max_2_1 = np.abs((np.argmax(data, axis = 1) + 180) - np.argmax(reconstructions_2, axis = 1))
+distance_from_max_2_2 = np.abs((np.argmax(data, axis = 1) - 180) - np.argmax(reconstructions_2, axis = 1))
+distance_from_max_2 = np.min([distance_from_max_2_0, distance_from_max_2_1, distance_from_max_2_2], axis = 0)
+roughness = np.abs(np.mean(np.diff(reconstructions, axis = 1), axis = 1))
+roughness_2 = np.abs(np.mean(np.diff(reconstructions_2, axis = 1), axis = 1))
 theta = np.argmax(data, axis = 1)
 
-b1 = ax[1].plot(np.arange(data.shape[0]), mse, label = 'Mean Squared Error')[0]
-c1 = ax[2].plot(np.arange(data.shape[0]), distance_from_max, label = 'Distance from Max')[0]
-d1 = ax[3].plot(np.arange(data.shape[0]), roughness, label = 'Roughness')[0]
-e1 = ax[4].plot(np.arange(data.shape[0]), theta, label = 'Theta')[0]
+b1 = ax[1].plot(np.arange(data.shape[0]), mse, label = 'Mean Squared Error (Centre)')[0]
+b2 = ax[1].text(0, 0.01, "Sum (C): {}".format(np.round(mse[0], 2)))
+b3 = ax[1].plot(np.arange(data.shape[0]), mse_2, label = 'Mean Squared Error (All)')[0]
+b4 = ax[1].text(250, 0.01, "Sum (A): {}".format(np.round(mse_2[0], 2)))
+c1 = ax[2].plot(np.arange(data.shape[0]), distance_from_max, label = 'Distance from Max (Centre)')[0]
+c2 = ax[2].text(0, 1, "Sum (C): {}".format(distance_from_max[0]))
+c3 = ax[2].plot(np.arange(data.shape[0]), distance_from_max_2, label = 'Distance from Max (All)')[0]
+c4 = ax[2].text(250, 1, "Sum (A): {}".format(distance_from_max_2[0]))
+d1 = ax[3].plot(np.arange(data.shape[0]), roughness, label = 'Roughness (Centre)')[0]
+d2 = ax[3].text(0, 0.02, "Sum (C): {}".format(roughness[0]))
+d3 = ax[3].plot(np.arange(data.shape[0]), roughness_2, label = 'Roughness (All)')[0]
+d4 = ax[3].text(250, 0.02, "Sum (A): {}".format(roughness_2[0]))
+e1 = ax[4].plot(np.arange(data.shape[0]), theta, label = 'Active Neuron')[0]
+e2 = ax[4].text(0, 0.02, "{}".format(theta[0] % 2*np.pi))
 
 ax[0].legend()
 ax[1].legend()
@@ -62,12 +85,14 @@ ax[3].legend()
 ax[4].legend()
 
 
-# ax[1].set_xlim(0, data.shape[0])
-# ax[1].set_ylim(0, np.max(mse))
-# ax[2].set_xlim(0, data.shape[0])
-# ax[2].set_ylim(0, np.max(distance_from_max))
-# ax[3].set_xlim(0, data.shape[0])
-# ax[3].set_ylim(0, np.max(roughness))
+ax[1].set_xlim(0, data.shape[0])
+ax[1].set_ylim(0, np.max(mse))
+ax[2].set_xlim(0, data.shape[0])
+ax[2].set_ylim(0, np.max(distance_from_max))
+ax[3].set_xlim(0, data.shape[0])
+ax[3].set_ylim(0, np.max(roughness))
+ax[4].set_xlim(0, data.shape[0])
+ax[4].set_ylim(0, 180)
 
 def animate(i):
 
@@ -75,19 +100,31 @@ def animate(i):
     a2.set_data(np.arange(data.shape[1]), reconstructions[i,:])
     a3.set_data(np.argmax(reconstructions[i,:]), [0,1])
     a4.set_text("{}/{}".format(i, len(data)))
+    a5.set_data(np.arange(data.shape[1]), reconstructions_2[i,:])
+    a6.set_data(np.argmax(reconstructions_2[i,:]), [0,1])
 
     # ax[1].scatter(i, mse[i])
     # ax[2].scatter(i, distance_from_max[i])
     # ax[3].scatter(i, roughness[i])
 
     b1.set_data(np.arange(0, i), mse[:i])
+    b2.set_text("Sum (C): {}".format(np.round(np.sum(mse[:i]), 2)))
+    b3.set_data(np.arange(0, i), mse_2[:i])
+    b4.set_text("Sum (A): {}".format(np.round(np.sum(mse_2[:i]), 2)))
     c1.set_data(np.arange(0, i), distance_from_max[:i])
+    c2.set_text("Sum (C): {}".format(np.sum(distance_from_max[:i])))
+    c3.set_data(np.arange(0, i), distance_from_max_2[:i])
+    c4.set_text("Sum (A): {}".format(np.sum(distance_from_max_2[:i])))
     d1.set_data(np.arange(0, i), roughness[:i])
+    d2.set_text("Sum (C): {}".format(np.sum(roughness[:i])))
+    d3.set_data(np.arange(0, i), roughness_2[:i])
+    d4.set_text("Sum (A): {}".format(np.sum(roughness_2[:i])))
     e1.set_data(np.arange(0, i), theta[:i])
+    e2.set_text("{}".format(theta[i]))
 
-    return a1, a2, a3, a4, b1, c1, d1, e1
+    return a1, a2, a3, a4, a5, a6, b1, c1, d1, e1
 
-animation = ani.FuncAnimation(fig, animate, len(data), repeat = True)
+animation = ani.FuncAnimation(fig, animate, len(data), repeat = False)
 
 #animation.save(reconstructions_folder + "animated_hd.gif")
 
