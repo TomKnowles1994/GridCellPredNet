b1de9acbc47c21e91d58a44be30c9c216333b0f2
TomKnowles1994 <71404211+TomKnowles1994@users.noreply.github.com>
2023-03-01 18:20:44 +0000

    Converging and with inference option

--- som_no_keras.py
+++ som_no_keras.py
@@ -12,11 +12,14 @@ from matplotlib.animation import FuncAnimation
 
 colourtest = False
 
-def rings_to_som(data_path, sample_count = 100, map_dimensions = (9,9), topology = 'square', ring_scale = 30, ring_type = 'index', neighbourhood = 5, epochs = 100, learning_rate = 0.01, wraparound = False):
+show_gradients = False
+
+def rings_to_som(data_path, sample_count = 100, map_dimensions = (10,10), topology = 'square', 
+                 ring_scale = 30, ring_type = 'index', neighbourhood = 5, neighbourhood_decay = 1,
+                 epochs = 100, learning_rate = 0.01, learning_rate_decay = 1, wraparound = False,
+                 mode = 'training', weights = None):
 
     input_data = np.array([np.load(data_path + 'ring_{}_gaussians_{}.npy'.format(i, ring_scale)) for i in (1,2,3)])[:,:sample_count]
-
-    #print(input_data.shape)
 
     if ring_type == 'index' and len(input_data.shape) > 2:
     
@@ -24,7 +27,7 @@ def rings_to_som(data_path, sample_count = 100, map_dimensions = (9,9), topology
 
         input_data = np.argmax(input_data, axis = -1)
 
-        input_data = input_data / ring_scale
+        input_data = (input_data - np.min(input_data)) / (np.max(input_data) - np.min(input_data))
 
     else:
 
@@ -38,21 +41,17 @@ def rings_to_som(data_path, sample_count = 100, map_dimensions = (9,9), topology
 
     print(input_data.shape)
 
-    random_generator = np.random.default_rng()
+    if weights is None:
 
-    map_weights = random_generator.random(size = (*map_dimensions, *input_data.shape[1:]))
+        random_generator = np.random.default_rng()
 
-    # map_weight_means = random_generator.random((*map_dimensions, *input_data.shape[1:-1]))
+        map_weights = random_generator.random(size = (*map_dimensions, *input_data.shape[1:]))
 
-    # print(map_weight_means.shape)
+    else:
 
-    # map_weights = norm.pdf(np.mgrid[0:map_dimensions[0], 0:map_dimensions[1], 0:input_data.shape[1], 0:input_data.shape[2]], loc = map_weight_means)
-
-    #print(map_weights.shape)
+        map_weights = weights
 
     map_indices = np.dstack(np.indices(dimensions = map_dimensions))[:,:,::-1]
-
-    #print(map_indices.shape)
 
     neighbourhood = neighbourhood
 
@@ -60,140 +59,180 @@ def rings_to_som(data_path, sample_count = 100, map_dimensions = (9,9), topology
 
     frames = []
 
-    for epoch in range(epochs):
+    theta = np.random.default_rng()
 
-        theta = np.random.default_rng()
+    if mode == 'training':
 
-        theta_choice = theta.random()
+        for epoch in range(epochs):
+            
+            input_data = theta.permutation(input_data, axis = 0)
 
-        #if True:
-        for item in input_data:
+            for item in input_data:
 
-            if theta_choice > 0.75:
+                #if theta.random() > 0.75:
+                if True:
 
-            #if True:
-                fig, ax = plt.subplots(3,3)
+                    difference =                            map_weights - item
 
-                #item = input_data[0]
+                    input_distance =                        np.linalg.norm(difference, ord = 2, axis = -1)
 
-                difference =                            map_weights - item
+                    winning_node_flat =                     np.argmin(input_distance.flatten())
 
-                input_distance =                        np.linalg.norm(difference, ord = 2, axis = -1)
+                    winning_node_x =                        winning_node_flat // map_dimensions[0]
 
-                winning_node_flat =                     np.argmin(input_distance.flatten())
+                    winning_node_y =                        winning_node_flat % map_dimensions[0]
 
-                winning_node_x =                        winning_node_flat // map_dimensions[0]
+                    winning_node =                          (winning_node_x, winning_node_y)
 
-                winning_node_y =                        winning_node_flat % map_dimensions[0]
+                    weight_difference =                     np.abs(map_indices - winning_node)
 
-                winning_node =                          (winning_node_x, winning_node_y)
+                    if wraparound:
 
-                for artist in ax.flatten():
+                        weight_difference_left =            np.abs(map_indices - (winning_node + np.array(map_dimensions)))
+                        weight_difference_right =           np.abs(map_indices - (winning_node - np.array(map_dimensions)))
 
-                    artist.scatter(*winning_node, c = 'red')
+                        weight_difference =                 np.min([weight_difference, weight_difference_left, weight_difference_right], axis = 0)
 
-                weight_difference =                     np.abs(map_indices - winning_node)
+                    winning_node_distance =                 np.linalg.norm(weight_difference, ord = 2, axis = -1)
 
-                if wraparound:
+                    winning_node_distance_gaussian =        np.exp((-winning_node_distance ** 2) / (neighbourhood ** 2))
 
-                    weight_difference_left =            np.abs(map_indices - (winning_node + np.array(map_dimensions)))
-                    weight_difference_right =           np.abs(map_indices - (winning_node - np.array(map_dimensions)))
+                    gradients =                             learning_rate * difference * winning_node_distance_gaussian[..., None]
 
-                    weight_difference =                 np.min([weight_difference, weight_difference_left, weight_difference_right], axis = 0)
+                    if show_gradients:
 
-                winning_node_distance =                 np.linalg.norm(weight_difference, ord = 2, axis = -1)
+                        fig, ax = plt.subplots(4,3)
 
-                winning_node_distance_gaussian =        np.exp((-winning_node_distance ** 2) / (2 * neighbourhood ** 2))
+                        for artist in ax.flatten():
 
-                ax[0][0].imshow(((winning_node_distance - np.min(winning_node_distance)) / (np.max(winning_node_distance) - np.min(winning_node_distance))), vmin = 0, vmax = 1, origin = 'lower')
-                ax[0][1].imshow(((winning_node_distance - np.min(winning_node_distance)) / (np.max(winning_node_distance) - np.min(winning_node_distance))), vmin = 0, vmax = 1, origin = 'lower')
-                ax[0][2].imshow(((winning_node_distance - np.min(winning_node_distance)) / (np.max(winning_node_distance) - np.min(winning_node_distance))), vmin = 0, vmax = 1, origin = 'lower')
+                            artist.scatter(*winning_node, c = 'red')
 
-                gradients =                             learning_rate * difference * winning_node_distance_gaussian[..., None]
+                        ax[0][0].imshow(((winning_node_distance - np.min(winning_node_distance)) / (np.max(winning_node_distance) - np.min(winning_node_distance))), vmin = 0, vmax = 1, origin = 'lower')
+                        ax[0][1].imshow(((winning_node_distance - np.min(winning_node_distance)) / (np.max(winning_node_distance) - np.min(winning_node_distance))), vmin = 0, vmax = 1, origin = 'lower')
+                        ax[0][2].imshow(((winning_node_distance - np.min(winning_node_distance)) / (np.max(winning_node_distance) - np.min(winning_node_distance))), vmin = 0, vmax = 1, origin = 'lower')
 
-                ax[1][0].imshow(((gradients - np.min(gradients)) / (np.max(gradients) - np.min(gradients)))[...,0], vmin = 0, vmax = 1, origin = 'lower')
-                ax[1][1].imshow(((gradients - np.min(gradients)) / (np.max(gradients) - np.min(gradients)))[...,1], vmin = 0, vmax = 1, origin = 'lower')
-                ax[1][2].imshow(((gradients - np.min(gradients)) / (np.max(gradients) - np.min(gradients)))[...,2], vmin = 0, vmax = 1, origin = 'lower')
+                        ax[0][0].set_ylabel("Distance")
 
-                for artist in ax.flatten():
+                        ax[1][0].imshow(((winning_node_distance_gaussian - np.min(winning_node_distance_gaussian)) / (np.max(winning_node_distance_gaussian) - np.min(winning_node_distance_gaussian))), vmin = 0, vmax = 1, origin = 'lower')
+                        ax[1][1].imshow(((winning_node_distance_gaussian - np.min(winning_node_distance_gaussian)) / (np.max(winning_node_distance_gaussian) - np.min(winning_node_distance_gaussian))), vmin = 0, vmax = 1, origin = 'lower')
+                        ax[1][2].imshow(((winning_node_distance_gaussian - np.min(winning_node_distance_gaussian)) / (np.max(winning_node_distance_gaussian) - np.min(winning_node_distance_gaussian))), vmin = 0, vmax = 1, origin = 'lower')
 
-                    gradients_sum =                     np.sum(gradients, axis = -1)
+                        ax[1][0].set_ylabel("Gaussian")
 
-                    print(gradients_sum.shape)
+                        for i in range(gradients.shape[-1]):
 
-                    gradients_flat =                    np.argmax(gradients_sum.flatten())
+                            ax[2][i].imshow(((gradients[..., i] - np.min(gradients[..., i])) / (np.max(gradients[..., i]) - np.min(gradients[..., i]))), vmin = 0, vmax = 1, origin = 'lower')
+                            
+                        ax[2][0].set_ylabel("\u0394W, raw")
 
-                    print(gradients_flat.shape)
+                        for artist in ax.flatten():
 
-                    gradients_x =                       gradients_flat // map_dimensions[0]
+                            gradients_sum =                     np.sum(gradients ** 2, axis = -1)
 
-                    gradients_y =                       gradients_flat % map_dimensions[0]
+                            print(gradients_sum.shape)
 
-                    gradient_max =                      (gradients_x, gradients_y)
+                            gradients_flat =                    np.argmax(gradients_sum.flatten())
 
-                    artist.scatter(*gradient_max, c = 'orange', s = 40)
+                            print(gradients_flat.shape)
 
-                print(gradient_max)
+                            gradients_x =                       gradients_flat // map_dimensions[0]
 
-                gradients[winning_node_distance > neighbourhood] =  0
+                            gradients_y =                       gradients_flat % map_dimensions[0]
 
-                ax[2][0].imshow(((gradients - np.min(gradients)) / (np.max(gradients) - np.min(gradients)))[...,0], vmin = 0, vmax = 1, origin = 'lower')
-                ax[2][1].imshow(((gradients - np.min(gradients)) / (np.max(gradients) - np.min(gradients)))[...,1], vmin = 0, vmax = 1, origin = 'lower')
-                ax[2][2].imshow(((gradients - np.min(gradients)) / (np.max(gradients) - np.min(gradients)))[...,2], vmin = 0, vmax = 1, origin = 'lower')
+                            gradient_max =                      (gradients_x, gradients_y)
 
-                map_weights = map_weights + gradients
+                            artist.scatter(*gradient_max, c = 'orange', s = 40)
 
-                plt.show()
+                        print(gradient_max)
 
-        #print(np.argmin(difference_summed))
-        #print(difference_summed.shape)
+                    gradients[winning_node_distance > neighbourhood] =  0
 
-        neighbourhood = neighbourhood * np.exp(-0.01)
-        learning_rate = learning_rate * np.exp(-0.01)
+                    if show_gradients:
+                        
+                        for i in range(gradients.shape[-1]):
 
-        #print(neighbourhood)
-        #print(learning_rate)
+                            ax[3][i].imshow(((gradients[..., i] - np.min(gradients[..., i])) / (np.max(gradients[..., i]) - np.min(gradients[..., i]))), vmin = 0, vmax = 1, origin = 'lower')
+                            
+                        ax[3][0].set_ylabel("\u0394W, clipped")
 
-        # print(difference.shape)
-        # print(winning_node)
-        # print(xy_difference.shape)
+                    map_weights = map_weights + gradients
 
-        # fig, ax = plt.subplots(1,3)
+                    for i in range(map_weights.shape[-1]):
 
-        # ax[0].imshow(difference[..., 0])
-        # ax[1].imshow(difference[..., 1])
-        # ax[2].imshow(difference[..., 2])
+                        map_weights[:,:,i] = (map_weights[:,:,i] - np.min(map_weights[:,:,i])) / (np.max(map_weights[:,:,i]) - np.min(map_weights[:,:,i]))
 
-        # plt.show()
-        frames.append(map_weights)
+                    plt.show()
 
-        #print("Max map weight: {}".format(np.max(map_weights)))
-        #print("Min map weight: {}".format(np.min(map_weights)))
+            if neighbourhood > 1:# and epoch > epochs // 10:
 
-    #print(euclidean_distance.shape)
-    #print(euclidean_distance_sum.shape)
-    #print(gaussian_difference)
+                neighbourhood = neighbourhood * np.exp(-neighbourhood_decay)
 
-    return frames
+            if learning_rate > 0.000001:# and epoch > epochs // 10:
+
+                learning_rate = learning_rate * np.exp(-learning_rate_decay)
+
+            frames.append(map_weights)
+
+        return frames
+
+    elif mode == 'inference':
+
+        inferences = np.zeros(shape = (input_data.shape))
+
+        for i, item in enumerate(input_data):
+
+            difference =                            map_weights - item
+
+            input_distance =                        np.linalg.norm(difference, ord = 2, axis = -1)
+
+            winning_node_flat =                     np.argmin(input_distance.flatten())
+
+            winning_node_x =                        winning_node_flat // map_dimensions[0]
+
+            winning_node_y =                        winning_node_flat % map_dimensions[0]
+
+            winning_node =                          (winning_node_x, winning_node_y)
+
+            inferences[i, :] =                      map_weights[winning_node[0], winning_node[1], :]
+
+        return inferences
 
 data_path = 'C:/Users/Tom/Downloads/HBP/multimodalplacerecognition_datasets/whiskeye_oracle_datasets/real_rat/'
 
-frames = rings_to_som(data_path, sample_count = 100, map_dimensions = (10, 10), 
-                      neighbourhood = 5, learning_rate = 0.01, topology = 'square', 
-                      wraparound = False)
+# frames = rings_to_som(  data_path, epochs = 5, sample_count = 3000, map_dimensions = (10, 10), 
+#                         neighbourhood = 5, neighbourhood_decay = 0.1, 
+#                         learning_rate = 0.1, learning_rate_decay = 0.1,
+#                         topology = 'square', wraparound = True)
+
+frames = rings_to_som(  data_path, epochs = 100, sample_count = 3000, map_dimensions = (10, 10), 
+                        neighbourhood = 5, neighbourhood_decay = 0.001, 
+                        learning_rate = 0.01, learning_rate_decay = 0.01,
+                        topology = 'square', wraparound = True, mode = 'training')
 
 fig, ax = plt.subplots(1,4)
 
-#print("Display frames shape: {}".format(frames[0][...,0].shape))
+map1 = ax[0].imshow(frames[0][...,0], cmap = 'Reds', vmin = 0, vmax = 1, origin = 'lower')
+map2 = ax[1].imshow(frames[0][...,1], cmap = 'Greens', vmin = 0, vmax = 1, origin = 'lower')
+map3 = ax[2].imshow(frames[0][...,2], cmap = 'Blues', vmin = 0, vmax = 1, origin = 'lower')
+map4 = ax[3].imshow(frames[0][...,:], vmin = 0, vmax = 1, origin = 'lower')
 
-map1 = ax[0].imshow(frames[0][...,0])
-map2 = ax[1].imshow(frames[0][...,1])
-map3 = ax[2].imshow(frames[0][...,2])
-map4 = ax[3].imshow(frames[0][...,:])
+if colourtest is False:
+
+    ax[0].set_xlabel("Ring 1")
+    ax[1].set_xlabel("Ring 2")
+    ax[2].set_xlabel("Ring 3")
+    ax[3].set_xlabel("State Space")
+
+elif colourtest is True:
+
+    ax[0].set_xlabel("Red")
+    ax[1].set_xlabel("Green")
+    ax[2].set_xlabel("Blue")
+    ax[3].set_xlabel("Colour Space")
 
 def animate_som_learning(i):
 
-    #print(i)
+    print("Epoch {}/{}".format(i+1, len(frames)), end = '\r')
 
     map1.set_data(frames[i][...,0])
     map2.set_data(frames[i][...,1])
@@ -202,11 +241,6 @@ def animate_som_learning(i):
 
     return map1, map2, map3, map4
 
-#assert not np.array_equal(frames[0], frames[1])
-
-#print("Frame 0: {}".format(frames[0]))
-#print("Frame 1: {}".format(frames[1]))
-
-ani = FuncAnimation(fig, animate_som_learning, frames = range(len(frames)), interval = 100, blit = True)
+ani = FuncAnimation(fig, animate_som_learning, frames = range(len(frames)), interval = 100, blit = True, repeat = False)
 
 plt.show()
